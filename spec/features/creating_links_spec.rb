e01c5362180afd8b5ebce0a23ecfdc8719a4618c
Alain Lemaire <jaxdid@gmail.com>
2016-02-09 18:57:55 +0100

    Added DatabaseCleaner and updated tests to wipe database before and after each test.

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,5 +1,7 @@
+
 feature 'create links form' do
   scenario 'add title and url' do
+    DatabaseCleaner.clean
     visit('/links/new')
     fill_in(:title, with: "Google")
     fill_in(:url, with: "http://google.com")
