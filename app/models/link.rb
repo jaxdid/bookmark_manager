9a5e6a72ed4d668c5aa2ff102eb351e90fb27b69
Alain Lemaire <jaxdid@gmail.com>
2016-02-11 19:34:27 +0100

    Added DATABASE_URL env to Link model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
+ENV['DATABASE_URL'] = 'postgres://rsxehhkyrqagct:JcwqDMpEUz2_VWnY3rNjZ55kRX@ec2-54-83-17-9.compute-1.amazonaws.com:5432/d9bin54phqtct9'
+
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
@@ -10,7 +12,7 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.finalize
