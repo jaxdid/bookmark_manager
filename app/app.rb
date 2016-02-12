52133df99cac189411ea890c5679f5254eb4ad5a
Reiss Johnson <reissjohnson@me.com>
2016-02-12 11:58:42 +0000

    refactored to fix small error

--- app/app.rb
+++ app/app.rb
@@ -24,7 +24,7 @@ class BookmarkManager < Sinatra::Base
 
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
-    @links= tag ? tag.links : r[]
+    @links= tag ? tag.links : []
     erb :index
   end
 
